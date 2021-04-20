1fcf212c26fe7ab1a5b79a7ea8610495331b8cbd
strob <s1910456032@students.fh-hagenberg.at>
2021-04-20 12:32:01 +0200

    erstellung contact-form; main.js und contactform.css

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function showForm(){
+    console.log("hi");
+    document.getElementById("pop-up").classList.remove("invisible");
+}
+
+function closeForm(){
+    console.log("hi");
+    document.getElementById("pop-up").classList.add("invisible");
+}
