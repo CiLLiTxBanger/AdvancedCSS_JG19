6759e38258c3404d6119f9b77eab85c45bda7214
Jo Eder <jojo.eder@gmail.com>
2021-04-21 11:00:10 +0200

    dark mode lets go

--- js/main.js
+++ js/main.js
@@ -13,3 +13,29 @@ function showInquiry(){
 function closeInquiry(){
     document.getElementById("pop-up-anfrage").classList.add("invisible");
 }
+
+/* DARK MODE */
+function darkmode(){
+    let body = document.body;
+    body.classList.toggle("darkmode--general");
+
+    let cards = document.getElementsByClassName("card h-100");
+    for (let i = 0; i < cards.length; i++)
+    {
+        cards[i].classList.toggle("darkmode--card");
+    }
+
+    let team = document.getElementById("team--text__dark");
+    team.classList.toggle("darkmode--general");
+    team.classList.toggle("darkmode--shadow");
+
+
+    let member = document.getElementsByClassName("team--display");
+    for (let i = 0; i < member.length; i++)
+    {
+        member[i].classList.toggle("darkmode--shadow");
+    }
+
+    let contactData = document.getElementById("contactData--dark");
+    contactData.classList.toggle("darkmode--contactData");
+}
